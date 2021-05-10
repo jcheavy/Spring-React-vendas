371e36744c1cf680319cbb21e36d13f3e3224b49
jcheavy <jcheavy@hotmail.com>
2021-05-10 20:38:04 -0300

    add componentes saticos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer =()=> {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https:heavyusers.com.br" target="_blank" rel="noreferrer">Heavy Users</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https:heavyusers.com.br" target="_blank" rel="noreferrer">@jcheavy@hotmail.com</a></small></p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
